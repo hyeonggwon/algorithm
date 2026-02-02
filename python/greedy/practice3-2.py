eac552e407ff32064c49d03a64f7e4ae267f02d5
hyeonggwon <asdhjk98@naver.com>
2026-02-02 23:29:20 +0900

    practice 260202 : greedy

--- python/greedy/practice3-2.py
+++ python/greedy/practice3-2.py
@@ -1,15 +1,58 @@
-N, M, K = map(int, input().split())
-numbers = list(map(int, input().split()))
-numbers.sort()
+# N, M, K = map(int, input().split())
+# numbers = list(map(int, input().split()))
+# numbers.sort()
+#
+# first_largest = numbers[-1]
+# second_largest = numbers[-2]
+#
+# # Number of times the repeating block (K times first, 1 time second) is used
+# num_blocks = M // (K + 1)
+# # Number of remaining additions, which will be the largest number
+# remainder = M % (K + 1)
+#
+# result = num_blocks * (first_largest * K + second_largest) + remainder * first_largest
+#
+# print(result)
 
-first_largest = numbers[-1]
-second_largest = numbers[-2]
 
-# Number of times the repeating block (K times first, 1 time second) is used
-num_blocks = M // (K + 1)
-# Number of remaining additions, which will be the largest number
-remainder = M % (K + 1)
 
-result = num_blocks * (first_largest * K + second_largest) + remainder * first_largest
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+n, m, k = map(int, input().split())
+arr = list(map(int, input().split()))
+
+arr.sort(reverse=True)
+
+num_blocks = m // (k + 1)
+remainder = m % (k + 1)
+result = arr[0] * num_blocks * k + arr[1] * num_blocks + arr[0] * remainder
 print(result)
--- python/greedy/practice3-4.py
+++ python/greedy/practice3-4.py
@@ -1,11 +1,40 @@
+# n, k = map(int, input().split())
+# result = 0
+# while True:
+#     if n == n % k:
+#         break
+#     result += n % k
+#     n -= n % k
+#     result += 1
+#     n //= k
+# result += n - 1
+# print(result)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 n, k = map(int, input().split())
-result = 0
-while True:
-    if n == n % k:
-        break
-    result += n % k
-    n -= n % k
-    result += 1
-    n //= k
-result += n - 1
-print(result)
+cnt = 0
+while n > 1:
+    if n % k == 0:
+        n /= k
+    else:
+        n -= 1
+    cnt += 1
+print(cnt)
