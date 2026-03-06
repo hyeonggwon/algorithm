57a84ce7c9df07ac8eb28be1b34a528ef1c40e98
hyeonggwon <asdhjk98@naver.com>
2026-03-07 01:24:14 +0900

    description deleted

--- python/dfs&bfs/backjoon13549.py
+++ python/dfs&bfs/backjoon13549.py
@@ -6,14 +6,10 @@ def bfs(start):
     dp[start] = 0
     while queue:
         x = queue.popleft()
-
-        # Teleport has 0 cost, add to front of deque
         teleport_nx = 2 * x
         if 0 <= teleport_nx <= 100_000 and dp[teleport_nx] > dp[x]:
             dp[teleport_nx] = dp[x]
             queue.appendleft(teleport_nx)
-
-        # Walk has 1 cost, add to back of deque
         for walk_nx in [x - 1, x + 1]:
             if 0 <= walk_nx <= 100_000 and dp[walk_nx] > dp[x] + 1:
                 dp[walk_nx] = dp[x] + 1
