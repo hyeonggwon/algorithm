e9cf970d7a442d1f5b7d63b87e94ea82a1a4e877
hyeonggwon <asdhjk98@naver.com>
2026-03-09 01:34:38 +0900

    time complexity added

--- python/greedy/backjoon2212.py
+++ python/greedy/backjoon2212.py
@@ -1,6 +1,10 @@
 import sys
 import heapq
 
+# 목적은 최소 거리를 구하는 것이기 때문에, 센서 사이의 거리가 긴 것들을 K - 1 개 만큼 빼면 됨
+# 먼저 센서 위치를 정렬한 후,
+# 센서 사이의 거리를 우선순위 큐에 넣어서, 가장 거리가 큰 것들을 빼서 풀이
+# 시간복잡도 : O(n log n)
 input = sys.stdin.readline
 N = int(input())
 K = int(input())
